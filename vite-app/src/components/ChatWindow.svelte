9311dc799a5da60c037d079b4537175b27bf6bf3
EverydayApps <michael@everydayapps.com>
2023-07-01 16:13:34 -0400

    Update ChatWindow.svelte
    Got rid or unused code.

--- vite-app/src/components/ChatWindow.svelte
+++ vite-app/src/components/ChatWindow.svelte
@@ -1,137 +1,28 @@
 <!-- /frontend/src/components/ChatWindow.svelte -->
 
 <script>
-	
-
 	import { onMount } from "svelte";
 
 	class ChatClient {
-	constructor() {
-		this.messages = [];
-		this.newMessage = "";
-	}
-
-	/**
-	 * Post a message to the chat server.
-	 * @param {string} message
-	 */
-	async postChatMessage(message) {
-		if (!this.newMessage) throw new Error("Message is empty");
-		if (this.newMessage.length === 0) throw new Error("Message is empty");
-
-		const response = await fetch("http://localhost:8000/chat", {
-				method: "POST",
-				headers: { "Content-Type": "application/json" },
-				body: JSON.stringify({ message: this.newMessage }),
-			});
-		return response;
-	}
-
-	/**
-	 * Send a message to the chat server and process the response.
-	 */
-	/*
-	async sendChatMessageAsync() {
-		try {
-			if (!this.newMessage) return;
-
-			const response = await this.postChatMessage(this.newMessage);
-			this.messages = [...this.messages, { role: "user", content: this.newMessage }];
-			//this.newMessage = "";
-			const reader = response.body.getReader();
-			let chunks = "";
-			while (true) {
-				const { done, value } = await reader.read();
-				if (done) break;
-				chunks += new TextDecoder("utf-8").decode(value);
-				const endOfMessageIndex = chunks.indexOf("\n\n");
-				if (endOfMessageIndex !== -1) {
-					const messageString = chunks.slice(0, endOfMessageIndex);
-					chunks = chunks.slice(endOfMessageIndex + 2);
-					const data = JSON.parse(messageString);
-					const botMessage = data.choices[0].delta.content;
-					if (botMessage) {
-						if (this.messages[this.messages.length - 1].role === "user") {
-							this.messages = [
-								...this.messages,
-								{ role: "bot", content: botMessage },
-							];
-						} else {
-							this.messages[this.messages.length - 1].content += botMessage;
-						}
-						console.log(botMessage);
-					}
-					if (data.choices[0].finish_reason === "stop") {
-						console.log("Conversation ended");
-						break;
-					}
-				}
-			}
-		} catch (error) {
-			console.error(error);
+		constructor() {
+			this.messages = [];
+			this.newMessage = "";
 		}
-	}
-	*/
 
-}
-	
-	let chatClient = new ChatClient();
+		/**
+		 * Post a message to the chat server.
+		 * @param {string} message
+		 */
+		async postChatMessage(message) {
+			if (!this.newMessage) throw new Error("Message is empty");
+			if (this.newMessage.length === 0) throw new Error("Message is empty");
 
-	// Add the 'messages' variable with the new user or bot messages.
-	/**
-	 * @param {string} message
-	 * @param {string} role
-	 */
-	function addChatMessages(message, role) {
-		chatClient.messages = [...chatClient.messages, { role: role, content: message }, ];
-	}
-
-	// Update the 'messages' variable with the new user or bot messages.
-	/**
-	 ** @param {string} message
-	 ** @param {string} role
-	 **/
-	function updateChatMessages(message, role) {
-		if (chatClient.messages[chatClient.messages.length - 1].role !== role) {
-			throw new Error("The last message is not from the same role");
-		}
-		chatClient.messages[chatClient.messages.length - 1].content += message;
-	}
-
-	// Decode chunks of data received from the server and find the end of each message.
-	/**
-     * @param {ReadableStreamDefaultReader<Uint8Array>} reader
-     */
-	async function decodeChunks(reader) {
-		let chunks = "";
-		let utf8Decoder = new TextDecoder("utf-8");
-
-		while (true) {
-			const { done, value } = await reader.read();
-			if (done) break;
-
-			chunks += utf8Decoder.decode(value);
-			const endOfMessageIndex = chunks.indexOf("\n\n");
-
-			if (endOfMessageIndex !== -1) {
-				const messageString = chunks.slice(0, endOfMessageIndex);
-				chunks = chunks.slice(endOfMessageIndex + 2);
-
-				return { chunks, messageString };
-			}
-		}
-	}
-	
-	// Process the data in each message, including updating the 'messages' variable and handling the 'stop' finish reason.
-	/**
-     * @param {number} index
-     * @param {string} token
-     */
-	function processChatMessageData(index, token) {
-		if (index === 0) {
-			addChatMessages(token, "bot");
-		} else {
-			updateChatMessages(token, "bot");
+			const response = await fetch("http://localhost:8000/chat", {
+					method: "POST",
+					headers: { "Content-Type": "application/json" },
+					body: JSON.stringify({ message: this.newMessage }),
+				});
+			return response;
 		}
 	}
 
@@ -175,6 +66,10 @@
 			console.error(error);
 		}
 	}
+	
+	let chatClient = new ChatClient();
+
+	onMount(() => {});
 
 </script>
 
