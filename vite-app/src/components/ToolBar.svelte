69752d0ce1a7de6533fc0226f6944fe244aedbd0
EverydayApps <michael@everydayapps.com>
2023-07-13 12:59:52 -0400

    Some code cleanup.

--- vite-app/src/components/ToolBar.svelte
+++ vite-app/src/components/ToolBar.svelte
@@ -17,55 +17,41 @@
 <nav class="toolbar">
 {#if user}
 	<div class="items-center space-x-2 hidden xl:inline-flex">
-		<img
-			src={user?.photoURL}
-			alt={user?.displayName}
-			class="h-12 w-12 rounded-full" />
+		<img src={user?.photoURL}
+			 alt={user?.displayName}
+			 class="h-12 w-12 avatar circle" />
 		<div class="flex flex-col">
-			<p>
-				Logged in as <span class="text-purple-500 italic font-bold">
-					{user?.displayName}
-				</span>
-			</p>
+			<p>{user?.displayName}</p>
 			<p>{user?.email}</p>
 		</div>
 		<button on:click={logout} class="bg-white p-2 rounded-full text-black">Logout</button>
 	</div>
-	<!--button on:click={logout}>Logout</button-->
-	<!--div>Welcome, {user.displayName}</div-->
 {:else}
 	<button on:click={login}>Login with Google</button>
 {/if}
-	<!--ul class="toolbar-list">
-		<li class="toolbar-item"><a href="#link1" class="toolbar-link">Link 1</a></li>
-		<li class="toolbar-item"><a href="#link2" class="toolbar-link">Link 2</a></li>
-	</ul-->
 </nav>
 
 <style>
-nav.toolbar {
-	padding: 15px 0;
-	box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.1);
+
+img.avatar.circle {
+	height: 32px;
+	aspect-ratio: auto 32 / 32;
+	width: 32px;
 }
-/*
-.toolbar-list {
-	list-style-type: none;
-	margin: 0;
-	padding: 0;
-	display: flex;
-	justify-content: space-around;
+
+.avatar {
+	background-color: var(--color-avatar-bg);
+	border-radius: var(--borderRadius-medium, 6px);
+	box-shadow: 0 0 0 1px var(--color-avatar-border);
+	display: inline-block;
+	flex-shrink: 0;
+	line-height: 1;
+	overflow: hidden;
+	vertical-align: middle;
 }
-.toolbar-item {
-	padding: 0 10px;
+
+.circle {
+	border-radius: var(--borderRadius-full, 50%) !important;
 }
-.toolbar-link {
-	text-decoration: none;
-	color: #f8f9fa;
-	font-size: 16px;
-	transition: color 0.3s ease;
-}
-.toolbar-link:hover {
-	color: #007BFF;
-}
-*/
+
 </style>
--- vite-app/src/stores/auth.store.ts
+++ vite-app/src/stores/auth.store.ts
@@ -1,13 +1,24 @@
 
 import { writable, type Writable } from "svelte/store";
 import {
-	getAuth, onAuthStateChanged, GoogleAuthProvider, signInWithPopup, signOut,
-	type Auth, type User, type UserCredential
+	getAuth,
+	onAuthStateChanged,
+	GoogleAuthProvider,
+	signInWithPopup,
+	signOut,
+	type Auth,
+	type User,
+	type UserCredential
 } from "firebase/auth";
+import {
+	initializeApp,
+	getApp,
+	getApps,
+	type FirebaseOptions,
+	type FirebaseApp
+} from "firebase/app";
 
-import { initializeApp } from "firebase/app";
-
-const firebaseConfig = {
+const firebaseConfig: FirebaseOptions = {
 	apiKey: "AIzaSyABNk9oM5G2gZZy0_XrKAT-eAPI7q6feLs",
 	authDomain: "mbotfbase.firebaseapp.com",
 	projectId: "mbotfbase",
@@ -16,20 +27,26 @@ const firebaseConfig = {
 	appId: "1:311121570177:web:e6472526cb1d1eedaba080",
 	measurementId: "G-5GXRMHKLR5"
 };
-const firebaseApp = initializeApp(firebaseConfig)
+
+const firebaseApp: FirebaseApp = (getApps().length === 0) ?
+	initializeApp(firebaseConfig) : getApp();
+
 const auth: Auth = getAuth(firebaseApp);
+
 export const authentication: Writable<User | null> = writable(null);
 
-auth.onAuthStateChanged(user => {
-	if (user) {
-		authentication.set(user);
-		console.log("User is logged in.");
-		console.log(user);
-	} else {
-		authentication.set(null);
-		console.log("User is not logged in.");
-	}
-});
+//onMount(() => {
+	auth.onAuthStateChanged((user: User | null) => {
+		if (user) {
+			authentication.set(user);
+			console.log("User is logged in.");
+			console.log(user);
+		} else {
+			authentication.set(null);
+			console.log("User is not logged in.");
+		}
+	});
+//});
 /*
 export const authentication = writable({
 	isLoggedIn: false,
