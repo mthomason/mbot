f20a8113cf26309bd88f4d3fccd09bdc62bf833d
EverydayApps <michael@everydayapps.com>
2023-07-16 13:31:43 -0400

    Added `skeleton` for styling.

---
+++ frontend/src/routes/+layout.svelte
@@ -0,0 +1,22 @@
+<script>
+	import '@skeletonlabs/skeleton/themes/theme-skeleton.css';
+	import '@skeletonlabs/skeleton/styles/skeleton.css';
+	import '../app.postcss';
+	import { AppShell, AppBar, LightSwitch } from '@skeletonlabs/skeleton';
+	import Login from '../components/Login.svelte';
+</script>
+
+<AppShell>
+	<svelte:fragment slot="header">
+		<AppBar>
+			<svelte:fragment slot="lead">
+				💬🤖
+			</svelte:fragment>
+			<svelte:fragment slot="trail">
+				<LightSwitch /><Login />
+			</svelte:fragment>
+			<svelte:fragment slot="headline">mbot</svelte:fragment>
+		</AppBar>
+	</svelte:fragment>
+	<slot />
+</AppShell>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,27 +1,17 @@
 <!-- /frontend/src/routes/+page.svelte -->
 <script>
 	import ChatWindow from '../components/ChatWindow.svelte';
-	import NavBar from '../components/NavBar.svelte';
-	import ToolBar from '../components/ToolBar.svelte';
-	//import LeftNavBar from '../components/LeftNavBar.svelte';
-	//import BottomNavBar from '../components/BottomNavBar.svelte';
 </script>
 
-<ToolBar />
 <header class="bg-secondary p-4 flex justify-between items-center text-white">
 	<h1 class="font-bold text-sm md:text-2xl tracking-widest">mbot</h1>
 </header>
-<NavBar />
-<!--LeftNavBar /-->
 <main>
 	<div class="app-chat-block">
 		<ChatWindow />
 	</div>
 </main>
-<!--BottomNavBar /-->
-
-	<!-- @import '../styles/app.css'; -->
 
 <style>
-	@import '../styles/app.css';
+	/* @import '../styles/app.css'; */
 </style>
