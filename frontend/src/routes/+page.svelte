c90eb74c5f216d92d73d9b4a43207254f72fd2bd
EverydayApps <michael@everydayapps.com>
2023-10-11 07:06:03 -0400

    More style fixes.

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <div class="container p-10 space-y-4">
-	<header class="bg-secondary p-4 flex justify-between items-center text-white">
+	<header class="bg-secondary p-4 flex justify-between items-center">
 		<h1 class="font-bold text-sm md:text-2xl tracking-widest">mbot</h1>
 	</header>
 	<main>
